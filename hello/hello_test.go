3a0697cccd995095f8dbabecfc458a41fdf0c3b4
Corey Hinkle <bugg123@gmail.com>
2019-11-27 14:30:01 -0600

    Adding more tests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Corey")
-	want := "Hello Corey"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Saying hello to corey", func(t *testing.T) {
+		got := Hello("Corey")
+		want := "Hello Corey"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say Hello World when empty string provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
 }
