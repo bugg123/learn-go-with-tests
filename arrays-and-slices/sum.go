a9f13c86aaeb36c13c175608e44721ba35ed29d6
Corey Hinkle <bugg123@gmail.com>
2019-11-27 18:24:35 -0600

    Add arrays and slices

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -6,3 +6,18 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+	return
+}
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func assertEqual(actual, expected int, numbers []int, t *testing.T) {
 	if actual != expected {
@@ -14,5 +17,28 @@ func TestSum(t *testing.T) {
 		actual := Sum(numbers)
 		expected := 15
 		assertEqual(actual, expected, numbers, t)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sum all slices past in", func(t *testing.T) {
+		slice1 := []int{1, 2, 3, 4, 5}
+		slice2 := []int{1, 2, 3}
+		got := SumAll(slice1, slice2)
+		want := []int{15, 6}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but wanted %v", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("sum all tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 9}, []int{1, 2}, []int{1,2,3})
+		want := []int{9, 2, 5}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but wanted %v", got, want)
+		}
 	})
 }
