6ad551c323bb3c70c64d9a5c90e8866ff708abce
Corey Hinkle <bugg123@gmail.com>
2019-12-08 21:59:49 -0500

    Add reflection

---
+++ reflection/reflection_test.go
@@ -0,0 +1,120 @@
+package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Cat"},
+			[]string{"Cat"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Cat", "Morgantown"},
+			[]string{"Cat", "Morgantown"},
+		},
+		{
+			"Struct with non-string field",
+			struct {
+				Name string
+				Age  int
+			}{"Cat", 31},
+			[]string{"Cat"},
+		},
+		{
+			"Nested fields",
+			Person{
+				"Cat",
+				Profile{31, "Morgantown"},
+			},
+			[]string{"Cat", "Morgantown"},
+		},
+		{
+			"Pointers to things",
+			&Person{
+				"Cat",
+				Profile{33, "Morgantown"},
+			},
+			[]string{"Cat", "Morgantown"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
+	}
+
+	for _, test := range cases {
+
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
+	}
+}
