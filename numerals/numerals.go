cf5bb62c09d7c00e8d480e24620a0c6789bbe830
Corey Hinkle <bugg123@gmail.com>
2019-12-16 12:10:52 -0500

    Add numerals, half way through

---
+++ numerals/numerals.go
@@ -0,0 +1,75 @@
+package numerals
+
+import "strings"
+
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+type RomanNumerals []RomanNumeral
+
+var romanNumerals = RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func (r RomanNumerals) ValueOf(symbols ...byte) int {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+	return 0
+}
+
+func ConvertToRoman(input int) string {
+
+	var results strings.Builder
+
+	for _, numeral := range romanNumerals {
+		for input >= numeral.Value {
+			results.WriteString(numeral.Symbol)
+			input -= numeral.Value
+		}
+	}
+	return results.String()
+}
+
+func ConvertToArabic(input string) int {
+	total := 0
+
+	for i := 0; i < len(input); i++ {
+		symbol := input[i]
+
+		if CouldBeSubtractive(i, symbol, input) {
+			if value := romanNumerals.ValueOf(symbol, input[i+1]); value != 0 {
+				total += value
+				i++
+			} else {
+				total += romanNumerals.ValueOf(symbol)
+			}
+		} else {
+			total += romanNumerals.ValueOf(symbol)
+		}
+
+	}
+	return total
+}
+
+func CouldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
+	isSubtractiveSymbol := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
+	return index+1 < len(roman) && isSubtractiveSymbol
+}
